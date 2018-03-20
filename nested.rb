9f322991d73162317b6ed80fbe77d3f9c9a3dd14
Kachel Wittig <kjwittig@gmail.com>
2018-03-20 01:51:45 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,13 +15,7 @@ def hopper
         }
      }
 
-	hopper = {
-			:grace_hopper => {
-				:known_for => "COBOL",
-				:languages => ["COBOL", "FORTRAN"]
-			}
-		}
-	puts hopper[:grace_hopper]
+ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
